eaa0db6327dba672cb26d95fcd7a8bfd1b7673db
Evan Purcer <evanpurcer@gmail.com>
2016-03-25 12:00:20 -0700

    Improve check for whether app is running within platform

--- src/index.js
+++ src/index.js
@@ -5,26 +5,28 @@ import Panel from './models/panel';
 
 import './stylesheets/index.styl';
 
+const IS_WITHIN_MP_PLATFORM_IFRAME = window.parent !== window && window.parent.mixpanel;
+
 function initialize(attrs={}) {
-    let panel = new Panel();
+  let panel = new Panel();
 
-    const initialState = {
-      $screen: 'list',
-      panel,
-      panels: {[panel.id]: panel},
-    };
+  const initialState = {
+    $screen: 'list',
+    panel,
+    panels: {[panel.id]: panel},
+  };
 
-    return new PanelApp('app', initialState, attrs);
+  return new PanelApp('app', initialState, attrs);
 }
 
-if (window.parent === window) { // the app is running outside of an iframe
-    initialize().update();
-} else { // the app is running inside an iframe (within Mixpanel Platform)
-    const parentFrame = new Framesg(window.parent, 'panel-foundation', {
-      startApp: parentData => {
-        panelApp = initialize({parentFrame});
-        window.history.replaceState(null, null, parentData.hash.replace(/^#*/, '#'));
-        panelApp.update();
-      },
-    });
+if (IS_WITHIN_MP_PLATFORM_IFRAME) {
+  const parentFrame = new Framesg(window.parent, 'panel-foundation', {
+    startApp: parentData => {
+      panelApp = initialize({parentFrame});
+      window.history.replaceState(null, null, parentData.hash.replace(/^#*/, '#'));
+      panelApp.update();
+    },
+  });
+} else {
+  initialize().update();
 }
