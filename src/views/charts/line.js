8fc35ea38772a3790eecae903e7fbd028f01a0cf
Evan Purcer <evanpurcer@gmail.com>
2016-05-04 23:17:23 -0700

    Move line chart webcomponent into line chart view

--- src/views/charts/line.js
+++ src/views/charts/line.js
@@ -1,10 +1,35 @@
 import BaseView from '../base';
 
-import { register } from '../../elements/charts/line';
-register();
-
 import template from '../templates/charts/line.jade';
 import '../stylesheets/charts/line.styl';
+
+document.registerElement('irb-line-chart', class extends HTMLElement {
+  createdCallback() {
+    this.$el = $('<div>').appendTo(this);
+  }
+
+  attachedCallback() {
+    this.$el
+      .MPChart({chartType: 'line'})
+      .MPChart('setData', JSON.parse(this._data_string || {}));
+
+    this._chart_initialized = true;
+  }
+
+  get data() {
+    return JSON.parse(this._data_string);
+  }
+
+  set data(data) {
+    if (this._data_string !== data) {
+      this._data_string = data;
+
+      if (this._chart_initialized) {
+        this.$el.MPChart('setData', JSON.parse(data));
+      }
+    }
+  }
+});
 
 export default class LineChartView extends BaseView {
   get TEMPLATE() {
