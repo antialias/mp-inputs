6bef81e2c817c19592e67549895ed070e957aeab
Evan Purcer <evanpurcer@gmail.com>
2016-05-24 21:01:05 -0700

    clean up group component

--- src/irb-app/irb-builder/builder-group/index.js
+++ src/irb-app/irb-builder/builder-group/index.js
@@ -5,7 +5,7 @@ import { Component } from 'panel';
 import { extend, renameProperty } from '../../../util';
 
 import { AddControl, EditControl } from '../controls';
-import { Clause, ShowClause } from '../../../models/clause';
+import { Clause } from '../../../models/clause';
 import { Pane, PaneContent } from '../../pane';
 
 import template from './index.jade';
@@ -14,16 +14,7 @@ import './index.styl';
 
 document.registerElement('builder-group', class extends Component {
   get config() {
-    return {
-      template,
-
-      helpers: {
-        isDisabled: () => {
-          const showValues = this.state.sections.show.clauses.map(clause => clause.value);
-          return showValues.length > 1 || showValues.indexOf(ShowClause.TOP_EVENTS) !== -1;
-        },
-      },
-    };
+    return {template};
   }
 });
 
@@ -31,7 +22,7 @@ document.registerElement('builder-group', class extends Component {
 document.registerElement('group-add-control', class extends AddControl {
   get constants() {
     return extend(super.constants, {
-      label: 'Group',
+      label: 'Group by',
     });
   }
 
