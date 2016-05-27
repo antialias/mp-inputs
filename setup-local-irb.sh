bab894ad9b55bcc7e12046ed62c485942fd416d6
Evan Purcer <evanpurcer@gmail.com>
2016-05-26 18:27:43 -0700

    review feedback

--- setup-local-irb.sh
+++ setup-local-irb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 mkdir -p ../libs
 cd ../libs
-if cd mixpanel-platform; then git pull && cd ..; else git clone git@github.com:mixpanel/mixpanel-platform.git; fi
+if cd mixpanel-platform; then
+    git pull && cd ..
+else
+    git clone git@github.com:mixpanel/mixpanel-platform.git
+fi
 cd ../irb
 ln -s ../libs libs
