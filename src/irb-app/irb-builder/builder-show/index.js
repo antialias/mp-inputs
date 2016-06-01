a6588cb49e2de44148277ecc95f8344cb20bbfad
chi.zhang <chi.zhang@mixpanel.com>
2016-06-01 19:21:52 +0000

    fixup! IRB: added a button to reset query back to default.

--- src/irb-app/irb-builder/builder-show/index.js
+++ src/irb-app/irb-builder/builder-show/index.js
@@ -29,7 +29,7 @@ document.registerElement('show-add-control', class extends AddControl {
 
 document.registerElement('show-edit-control', class extends EditControl {
   shouldUpdate(state) {
-    return state.sections.getClause('show', this.clauseIndex);
+    return !!state.sections.getClause('show', this.clauseIndex);
   }
 
   get section() {
