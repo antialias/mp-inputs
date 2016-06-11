30d8791a75bc366da2ad62166e772abe517b2569
Jordan <jordan@mixpanel.com>
2016-06-10 17:02:43 -0700

    activate people/event/all in group pane

--- src/irb-app/irb-builder/controls/builder-pane.js
+++ src/irb-app/irb-builder/controls/builder-pane.js
@@ -111,6 +111,17 @@ document.registerElement('group-property-pane-content', class extends PaneConten
         },
         selectArrow: property => this.config.helpers.paneHandler(property, false),
         selectProperty: property => this.config.helpers.paneHandler(property, this.app.originStageClauseType() !== 'filter'),
+        TopProperties: (resourceType) => {
+          switch (resourceType) {
+            case 'all':
+              return this.state.topEventProperties.concat(this.state.topPeopleProperties).sort((a, b) => b.count - a.count);
+            case 'events':
+              return this.state.topEventProperties
+            case 'people':
+              return this.state.topPeopleProperties
+          }
+
+        },
       }),
     });
   }
