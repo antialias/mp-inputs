a0f7c860972319b14b3b472b4e30231672f9e7c4
teddddd <ted@mixpanel.com>
2016-06-18 01:09:30 +0000

    add calendar input and positioning outside pane dropdown

--- src/irb-app/irb-builder/controls/mp-calendar.js
+++ src/irb-app/irb-builder/controls/mp-calendar.js
@@ -1,15 +1,24 @@
 import Pikaday from 'pikaday';
 import WebComponent from 'webcomponent';
 
+import './mp-calendar.styl';
+
 document.registerElement('mp-calendar', class extends WebComponent {
   attachedCallback() {
+    this.inputEl = document.createElement('input');
+    this.inputEl.className = 'mp-pikaday-input';
+
     this.picker = new Pikaday({
+      field: this.inputEl,
+      container: document.getElementsByClassName('calendar-hook')[0],
       onSelect: date => {
         console.log('Selected', date);
         this.dispatchEvent(new Event('change', {detail: date}));
       },
     });
-    this.appendChild(this.picker.el);
+
+    this.appendChild(this.inputEl);
+    this.inputEl.focus();
   }
 
   // get value() {
