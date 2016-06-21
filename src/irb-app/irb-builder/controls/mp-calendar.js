e4551f90c71f56c484331efc4da8014cf4c53dc7
teddddd <ted@mixpanel.com>
2016-06-21 00:20:47 +0000

    remove calendar component cruft

--- src/irb-app/irb-builder/controls/mp-calendar.js
+++ src/irb-app/irb-builder/controls/mp-calendar.js
@@ -19,17 +19,4 @@ document.registerElement('mp-calendar', class extends WebComponent {
     this.appendChild(this.inputEl);
     this.inputEl.focus();
   }
-
-  // get value() {
-  //   return this.$el.val();
-  // }
-
-  // set value(value) {
-  //   const { from, to } = JSON.parse(value);
-  //   const formattedValue = {from: new Date(from), to: new Date(to)};
-
-  //   if (formattedValue !== this.$el.val()) {
-  //     this.$el.val(formattedValue);
-  //   }
-  // }
 });
