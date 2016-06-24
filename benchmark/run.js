1286c2ff32ce91aae6d10042446bcd254d3104e2
teddddd <ted@mixpanel.com>
2016-06-24 23:01:47 +0000

    install node-fetch for benchmark requests

--- benchmark/run.js
+++ benchmark/run.js
@@ -1,8 +1,12 @@
+import fetch from 'node-fetch';
+
 import BuilderSections from '../src/models/builder-sections';
 import { ShowClause, TimeClause } from '../src/models/clause';
 import { ShowSection, TimeSection } from '../src/models/section';
 import { MS_BY_UNIT } from '../src/models/queries/segmentation';
 import SegmentationQuery from '../src/models/queries/segmentation';
+
+const API_BASE = 'https://mixpanel.com';
 
 const irbQuery = new SegmentationQuery([]);
 irbQuery.query = irbQuery.buildQuery({
@@ -12,4 +16,9 @@ irbQuery.query = irbQuery.buildQuery({
   }),
 });
 
-console.log('args', irbQuery.buildJQLArgs());
+const postArgs = irbQuery.buildJQLArgs();
+for (const queryArgs of postArgs) {
+  const [url, params, options] = queryArgs;
+  const fullURL = `${API_BASE}/${url}`;
+  fetch(fullURL).then(res => res.text()).then(body => console.log(body));
+}
