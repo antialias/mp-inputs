127f2ce6cf5903b7a1b6a8fa6e8735b44b15c0f6
Jordan <jordan@mixpanel.com>
2016-06-23 17:31:09 -0700

    alphabetize helpers

--- src/irb-app/irb-result/mp-toast/index.js
+++ src/irb-app/irb-result/mp-toast/index.js
@@ -8,14 +8,14 @@ document.registerElement('mp-toast', class extends Component {
   get config() {
     return {
       helpers: {
-        trashToast: () => {
-          this.dispatchEvent(new Event('close'));
-        },
         eatToast: () => {
           this.dispatchEvent(new Event('select'));
         },
         getMessage: () => this.getAttribute('message'),
         getSelectText: () => this.getAttribute('select-text'),
+        trashToast: () => {
+          this.dispatchEvent(new Event('close'));
+        },
       },
       template,
     };
