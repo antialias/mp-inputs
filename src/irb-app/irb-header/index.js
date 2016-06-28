492bb878af71f017e84f2d4fac3529d05e904929
Jordan <jordan@mixpanel.com>
2016-06-28 16:25:10 -0700

    move download method to mixpanel-common

--- src/irb-app/irb-header/index.js
+++ src/irb-app/irb-header/index.js
@@ -1,4 +1,5 @@
 import { Component } from 'panel';
+import { downloadJQLQuery } from 'mixpanel-common/build/util';
 
 import './auto-sizing-input';
 
@@ -19,7 +20,7 @@ document.registerElement('irb-header', class extends Component {
         clickExportCSV: () => {
           if (!this.state.result.loading) {
             this.app.queries.segmentation.build(this.state).getParams().forEach(query => {
-              this.startDownload(query.script, `${this.state.reportName}`, query.params);
+              downloadJQLQuery(query.script, this.state.reportName, query.params);
             });
           }
         },
@@ -29,58 +30,4 @@ document.registerElement('irb-header', class extends Component {
     };
   }
 
-  htmlEncodeString(string) {
-    return string
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#039;')
-      .replace(/\r/g, '&#013;')
-      .replace(/\n/g, '&#010;')
-      ;
-  }
-
-  startDownload(script, filename, params=null, format='csv') {
-    // prepare form data
-    let parameters = {
-      script: script.replace(/\r/g, '').replace(/\n/g, '\r\n'),
-      download_file: `${filename}.${format}`,
-      params: params || {},
-    };
-    if (format === 'csv') {
-      parameters.format = 'csv';
-    }
-
-    const query = window.MP.api.getQueryOptions('/api/2.0/jql/', parameters, {type: 'POST'});
-
-    // prepare target iframe
-    this.idCounter = this.idCounter ? this.idCounter + 1 : 1;
-    const exportFrameID = `export-frame-${this.idCounter}`;
-    let exportFrame = document.createElement('iframe');
-    exportFrame.id = exportFrameID;
-    exportFrame.name = exportFrameID;
-    exportFrame.src = '';
-    exportFrame.style.display = 'none';
-    document.body.appendChild(exportFrame);
-
-    // submit request
-    let postForm = document.createElement('form');
-    postForm.action = query.endpoint;
-    postForm.method = 'POST';
-    postForm.style.display = 'none';
-    postForm.target = exportFrameID;
-    postForm.innerHTML = Object.keys(query.queryOptions.data).map(param => {
-      switch (param) {
-        case 'script':
-          return `<textarea name="script">${this.htmlEncodeString(parameters.script)}</textarea>`;
-        case 'params':
-          return `<input type="hidden" name="params" value="${this.htmlEncodeString(parameters.params)}"/>`;
-        default:
-          return `<input type="hidden" name="${param}" value="${query.queryOptions.data[param]}"/>`;
-      }
-    }).join('');
-    document.body.appendChild(postForm);
-    postForm.submit();
-  }
 });
