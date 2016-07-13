5841e66be48f46adbf44d46f03da2329a350e23a
chi.zhang <chi.zhang@mixpanel.com>
2016-07-13 22:17:57 +0000

    extrema: fix multiline object notation

--- src/models/queries/extrema.js
+++ src/models/queries/extrema.js
@@ -8,10 +8,12 @@ export default class ExtremaQuery extends BaseQuery {
 
   buildParams() {
     const params = extend(pick(this.query, ['event', 'on', 'allow_fewer_buckets', 'allow_more_buckets', 'buckets']),
-      { from_date: this.query.from.toISOString().split('T')[0],
+      {
+        from_date: this.query.from.toISOString().split('T')[0],
         to_date: this.query.to.toISOString().split('T')[0],
         interval: Math.min(36, this.query.interval),
-        cardinality_threshold: 50 });
+        cardinality_threshold: 50
+      });
     if (this.query.where) {
       params['selector'] = this.query.where;
     }
