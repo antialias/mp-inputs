5bc96ae5c4ba754fd4eeaacbe99136dbf6b176d2
chi.zhang <chi.zhang@mixpanel.com>
2016-07-18 22:22:41 +0000

    Jenkins: move everything into jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,13 @@
 # This script is run by Jenkins. See https://jenkins.gcp.corp.mixpanel.org/job/IRB/
 
+# set up
+wget -qO - https://deb.nodesource.com/setup_4.x | bash -
+apt-get install -y nodejs
+
+# copy scrects from kubernetes into the project
+cp /etc/secrets/irb/project-secrets.js benchmark/
+
+# run
 npm install
 npm run lint
 npm run build
