3342d52ea04610c9139e279e46007b13836a80d2
Jordan <jordan@mixpanel.com>
2016-07-25 16:49:04 -0600

    again, remove mediary functions

--- src/irb-app/irb-result/bar-chart/index.js
+++ src/irb-app/irb-result/bar-chart/index.js
@@ -42,7 +42,7 @@ document.registerElement('bar-chart', class extends Component {
         },
         onMouseEnter: (ev, name, value, percent) => {
           let hoverTooltip = util.extend(this.state.hoverTooltip, {show: true});
-          if (ev) {
+          if (ev && name) {
             const chartBounds = this.state.chartBoundaries;
             const targetBarBounds = ev.target.getBoundingClientRect();
             hoverTooltip = util.extend(hoverTooltip, {
