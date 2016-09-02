ac86978e8a88f619ae0998e63d761a007b16e63d
chi.zhang <chi.zhang@mixpanel.com>
2016-09-02 17:38:41 +0000

    extrema: actually add tests for extremaResultToBuckets

---
+++ test/extrema.js
@@ -0,0 +1,40 @@
+import expect from 'expect.js';
+
+import { extremaResultToBuckets } from '../src/models/queries/extrema.js';
+
+describe('extremaResultToBuckets', function() {
+    it('does not return buckets when no cardinality is present', function() {
+        const result = {};
+        expect(extremaResultToBuckets(result)).to.eql({});
+    });
+
+    it('does not return buckets when cardinality is not high', function() {
+        const result = {cardinality: 'low'};
+        expect(extremaResultToBuckets(result)).to.eql({});
+    });
+
+    it('handles >= 1 bucket_size correctly', function() {
+        const result = {
+            cardinality: 'high',
+            bucket_size: 20,
+            min: 0,
+            max: 100,
+        };
+        expect(extremaResultToBuckets(result)).to.eql({
+            buckets: [0, 20, 40, 60, 80, 100, 120],
+        });
+    });
+
+    it('handles < 1 bucket_size correctly', function() {
+        const result = {
+            cardinality: 'high',
+            bucket_size: 0.5,
+            min: 0,
+            max: 10,
+            multiplier: 4,
+        };
+        expect(extremaResultToBuckets(result)).to.eql({
+            buckets: [0, 2, 4, 6, 8, 10, 12],
+        });
+    });
+});
