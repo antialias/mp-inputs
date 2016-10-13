50359ef39d3a258cddd665c633476f1cbf0f9e8a
Ricky Doar <ricky@mixpanel.com>
2016-10-13 12:17:43 -0700

    updated eslint, fixed syntax

--- src/irb-app/irb-builder/controls/mp-calendar.js
+++ src/irb-app/irb-builder/controls/mp-calendar.js
@@ -59,7 +59,7 @@ document.registerElement(`mp-calendar`, class extends WebComponent {
         i18n: {
           months: [`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`],
           weekdays: [`Sunday`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`],
-          weekdaysShort: [`SU`,`MO`,`TU`,`WE`,`TH`,`FR`,`SA`],
+          weekdaysShort: [`SU`, `MO`, `TU`, `WE`, `TH`, `FR`, `SA`],
         },
         onSelect: date => this.selectDate(date, idx),
         theme: `pika-mixpanel`,
