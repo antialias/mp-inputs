f79170fb51dbf59becd8af76f4f747a739f11645
Ricky Doar <ricky@mixpanel.com>
2016-10-13 11:07:03 -0700

    removed calendar debug js

--- src/irb-app/irb-builder/controls/mp-calendar.js
+++ src/irb-app/irb-builder/controls/mp-calendar.js
@@ -64,11 +64,11 @@ document.registerElement(`mp-calendar`, class extends WebComponent {
         onSelect: date => this.selectDate(date, idx),
         theme: `pika-mixpanel`,
       });
-      // TODO TMP FOR DEVELOPMENT, REVERT ME
-      // el.picker.hide();
+      TODO TMP FOR DEVELOPMENT, REVERT ME
+      el.picker.hide();
 
       el.input.addEventListener(`focus`, () => el.picker.show());
-      // el.input.addEventListener(`blur`, () => el.picker.hide());
+      el.input.addEventListener(`blur`, () => el.picker.hide());
 
       this.appendChild(el.input);
 
