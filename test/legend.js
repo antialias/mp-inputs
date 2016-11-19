c5714171666be173d7f990b3d9714b40b75bc4a8
Jordan <jordan@mixpanel.com>
2016-11-18 17:18:31 -0800

    update tests for removing variable showLimits

--- test/legend.js
+++ test/legend.js
@@ -5,12 +5,9 @@ import { d2ResultsObj } from './fixtures';
 import Legend from '../src/models/legend';
 
 describe('Legend', function() {
-  beforeEach(function() {
-    this.legend = new Legend({});
-  });
-
   context('with default values', function() {
     beforeEach(function() {
+      this.legend = new Legend({});
       this.legend.updateLegendData(d2ResultsObj);
     });
 
@@ -18,7 +15,7 @@ describe('Legend', function() {
       expect(this.legend.seriesShowing).to.eql(['all', 'hidden']);
     });
 
-    it('sets the first 48 keys with highest numerical value to true', function() {
+    it('sets the first 24 keys with highest numerical value to true', function() {
       expect(this.legend.data[0].seriesName).to.eql('$browser');
       expect(this.legend.data[0].seriesData).to.eql({
         Chrome: true,
@@ -79,40 +76,6 @@ describe('Legend', function() {
         Firefox: 1,
         Opera: 2,
       });
-    });
-  });
-
-  context('with a showLimit of 2', function() {
-    beforeEach(function(){
-      this.legend.updateLegendData(d2ResultsObj, true, 2);
-      this.legend.buildColorMap();
-    });
-
-    it('sets the two highest numerical values to the default state', function() {
-      expect(this.legend.data[0].seriesName).to.eql('$browser');
-      expect(this.legend.data[0].seriesData).to.eql({
-        Chrome: true,
-        Safari: true,
-        Firefox: false,
-        Opera: false,
-      });
-      expect(this.legend.data[0].flattenedData).to.eql({
-        'Mac OS X Chrome': true,
-        'Mac OS X Firefox': false,
-        'Mac OS X Safari': false,
-        'Windows Chrome': true,
-        'Windows Firefox': false,
-        'Windows Opera': false,
-      });
-    });
-
-    it('gives color numbers to showing values', function() {
-      expect(this.legend.colorMap).to.eql({
-        Chrome: 1,
-        Safari: 2,
-      });
-      expect(this.legend.getColorForSeries('Windows Chrome', true)).to.eql(2);
-      expect(this.legend.getColorForSeries('Mac OS X Firefox', true)).to.eql(undefined);
     });
   });
 });
