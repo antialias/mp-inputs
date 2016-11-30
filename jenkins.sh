e804f2e3fdfd1d5343664f4608c556ac1a2e42a5
Chase Albert <chase.albert@mixpanel.com>
2016-11-30 11:31:59 -0800

    Set github status in jenkins.sh instead of jenkins proper.

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,35 @@
+#!/bin/bash
+set -o errexit -o pipefail -o nounset -o xtrace
 # This script is run by Jenkins. See https://jenkins.gcp.corp.mixpanel.org/job/IRB/
 
-set -e
+function github_status {
+    set +o xtrace
+    echo "setting github status to ${1}"
+    curl "https://api.github.com/repos/mixpanel-platform/irb/statuses/${GIT_COMMIT}" \
+        -H "Authorization: Bearer $(cat /etc/secrets/github-status/token)" \
+        -H "Content-type: application/json" \
+        -X POST -d @- <<EOF || true
+{
+    "context": "jenkins",
+    "description": "build ${1}",
+    "state": "${1}",
+    "target_url": "${BUILD_URL}"
+}
+EOF
+    set -o xtrace
+}
+
+function on_exit {
+    EC=$?
+    if [ $EC = 0 ]; then
+        github_status success
+    else
+        github_status failure
+    fi
+    exit $EC
+}
+trap on_exit EXIT
+github_status pending
 
 # set up
 wget -qO - https://deb.nodesource.com/setup_4.x | bash -
