af2d57e2e6d511d70dbabe84633768e2ee756de4
Evan Purcer <evanpurcer@gmail.com>
2017-01-04 23:49:41 -0800

    basic localstorage-based recently viewed functionality

--- src/irb-app/new-irb-builder/builder-show/builder-screen-events.js
+++ src/irb-app/new-irb-builder/builder-show/builder-screen-events.js
@@ -1,3 +1,5 @@
+/* global JSON, localStorage */
+
 import { BuilderScreenBase } from '../builder-pane/builder-screen-base';
 import { ShowClause } from '../../../models/clause';
 import {
@@ -9,14 +11,19 @@ import {
 import template from './builder-screen-events.jade';
 import './builder-screen-events.styl';
 
+const RECENT_EVENTS_LOCALSTORAGE_KEY = `mp_irb_recent_events`;
+
 document.registerElement(`builder-screen-events`, class extends BuilderScreenBase {
   get config() {
     return {
       template,
       helpers: extend(super.config.helpers, {
         getEvents: () => this.buildProgressiveList(),
-        getRecentEvents: () => this.buildProgressiveList().slice(2, 5),
-        clickedEvent: value => this.updateAndCommitStageClause({value}),
+        getRecentEvents: () => this.getRecentEvents(),
+        clickedEvent: value => {
+          this.updateRecentEvents(value);
+          this.updateAndCommitStageClause({value});
+        },
         clickedEventProperties: (ev, value) => {
           ev.stopPropagation();
           this.updateStageClause({value}, {shouldCommit: true});
@@ -32,5 +39,17 @@ document.registerElement(`builder-screen-events`, class extends BuilderScreenBas
     return [ShowClause.TOP_EVENTS, ShowClause.ALL_EVENTS].concat(sorted(this.state.topEvents, {
       transform: mpEvent => renameEvent(mpEvent.name).toLowerCase(),
     }));
+  }
+
+  getRecentEvents() {
+    return JSON.parse(localStorage.getItem(RECENT_EVENTS_LOCALSTORAGE_KEY) || `[]`);
+  }
+
+  updateRecentEvents(mpEvent) {
+    let recentEvents = this.getRecentEvents();
+    recentEvents = recentEvents.filter(recentEvent => recentEvent.name !== mpEvent.name);
+    recentEvents = [mpEvent].concat(recentEvents);
+    recentEvents = recentEvents.slice(0, 3);
+    localStorage.setItem(RECENT_EVENTS_LOCALSTORAGE_KEY, JSON.stringify(recentEvents));
   }
 });
