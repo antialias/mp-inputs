ff88e7f2f9a5cc32450c3ad30c442a42989b0064
Jordan <jordan@mixpanel.com>
2017-01-11 10:37:26 -0800

    header comlpetely on panel

--- src/irb-app/irb-result/chart-display/bar-chart/bar-chart-header/index.js
+++ src/irb-app/irb-result/chart-display/bar-chart/bar-chart-header/index.js
@@ -2,9 +2,10 @@
 
 import { Component } from 'panel';
 
-import * as util from '../../../../../util';
+import { abbreviateNumber, renameProperty } from '../../../../../util';
 
 import template from './index.jade';
+import './index.styl';
 
 const SORT_ICONS = {
   label: `alpha`,
@@ -20,8 +21,42 @@ document.registerElement(`irb-bar-chart-header`, class extends Component {
       },
       helpers: {
         SORT_ICONS,
-        clickedHeader: (section) => this.update({activeSortPanel: section}),
-        renameHeaderLabel: (header) => header === `$event` ? `Events` : util.renameProperty(header),
+        clickedHeader: section => {
+          const activeSortPanel = section === this.state.activeSortPanel ? null : section;
+          this.update({activeSortPanel});
+        },
+        clickedMaxValue: ev => {
+          ev.stopPropagation();
+          this.dispatchEvent(new CustomEvent(`change`, {detail: {axis: true, maxValueText: true}}));
+        },
+        getAxisSortIcon: () => {
+          let icon = ``;
+          if (this.state.sortConfig) {
+            if (this.state.sortConfig.colSortAttrs && this.state.displayOptions && this.state.displayOptions.plotStyle === `stacked`) {
+              const headerIdx = this.state.sortConfig.hideFirstSort ? 0 : this.state.headers.length;
+              const colAttrs = this.state.sortConfig.colSortAttrs[headerIdx];
+              if (colAttrs) {
+                icon = `sort-${SORT_ICONS[colAttrs.sortBy]}-${colAttrs.sortOrder}`;
+              }
+            } else if (this.state.sortConfig.sortBy === `value`) {
+              icon = `sort-value-${this.state.sortConfig.sortOrder}`;
+            }
+          }
+          return icon;
+        },
+        getHeaderSortActive: headerIdx => {
+          let icon = ``;
+          const sortConfig = this.state.sortConfig;
+          if (sortConfig && sortConfig.sortBy === `column` && !(sortConfig.hideFirstSort && headerIdx === 0)) {
+            const colAttrs = sortConfig.colSortAttrs[headerIdx];
+            if (colAttrs) {
+              icon = `sort-${SORT_ICONS[colAttrs.sortBy]}-${colAttrs.sortOrder}`;
+            }
+          }
+          return icon;
+        },
+        getMaxValueLabel: () => this.state.displayOptions.value === `absolute` ? abbreviateNumber(this.state.chartMax) : `%`,
+        renameHeaderLabel: header => header === `$event` ? `Events` : renameProperty(header),
         selectedHeaderSort: (type, colIdx, sortBy, sortOrder) => this.dispatchEvent(new CustomEvent(`change`, {
           detail: {
             colIdx,
@@ -59,7 +94,6 @@ document.registerElement(`irb-bar-chart-header`, class extends Component {
       chartLabel = `${chartLabel} ${functionLabel}`;
     }
 
-
     window.requestAnimationFrame(() => { // defer so we can inspect the fully-rendered table
 
       const headers = $(this).parents(`table`)
@@ -73,134 +107,12 @@ document.registerElement(`irb-bar-chart-header`, class extends Component {
 
       this.update({
         axisWidthStyle,
-        headers,
         chartLabel,
-      })
-    });
-  }
-
-  selectHeaderSort(type, colIdx, sortBy, sortOrder) {
-    this.dispatchEvent(new CustomEvent(`change`, {
-      detail: {
-        colIdx,
-        sortBy,
-        sortOrder,
-        type,
-      },
-    }));
-  }
-
-  hideAllSortHolders(ignoreElement) {
-    if (this.sortHolders) {
-      this.sortHolders.forEach(el => {
-        if (!el.is(ignoreElement)) {
-          el.removeClass(`active-sort-panel`);
-        }
+        chartMax: this.chartMax,
+        displayOptions: this.displayOptions,
+        headers,
+        sortConfig: this.sortConfig,
       });
-    }
-  }
-
-  render() {
-    // this.sortHolders = [];
-    // let headersEl = this;
-
-    // let headers = this.headers.map((header, idx) => {
-    //   const sortHolder = this.createSortHolder(`colSort`, [`label`, `value`], [`asc`, `desc`], idx);
-    //   this.sortHolders.push(sortHolder);
-    //   return $(`<div>`)
-    //     .addClass(`bar-chart-header`)
-    //     .data(`header-idx`, idx)
-    //     .on(`click`, () => {
-    //       this.hideAllSortHolders(sortHolder);
-    //       sortHolder.toggleClass(`active-sort-panel`);
-    //     })
-    //     .append($(`<div>`).addClass(`text`).html(
-    //       header === `$event` ? `Events` : util.renameProperty(header)
-    //     ))
-    //     .append($(`<div class="sort-icon sort-bar-header">`).append(headersEl.createSortIcon(idx)))
-    //     .append(sortHolder)
-    //     .get(0);
-    // });
-
-    // this.$el
-    //   .addClass(`loading`)
-    //   .append($headers);
-
-    // let chartTitle = this.chartLabel;
-    // if (this.functionLabel) {
-    //   chartTitle += ` ${this.functionLabel}`;
-    // }
-
-    // const sortAxisHolder = this.createSortHolder(`axisSort`, [`value`], [`asc`, `desc`]);
-    // this.sortHolders.push(sortAxisHolder);
-    // const $axisTitle = $(`<div>`)
-    //   .addClass(`axis-title`)
-    //   .append($(`<div>`).addClass(`text`).html(chartTitle))
-    //   .append($(`<div class="sort-icon sort-axis">`).append(headersEl.createAxisSortIcon()))
-    //   .append(sortAxisHolder);
-
-    // const $axisMaxValue = $(`<div>`)
-    //   .addClass(`text`)
-    //   .on(`click`, function(ev) {
-    //     headersEl.dispatchEvent(new CustomEvent(`change`, {detail: {axis: true, maxValueText: true}}));
-    //     ev.stopPropagation();
-    //   })
-    //   .html(this.displayOptions.value === `absolute` ? util.abbreviateNumber(this.chartMax) : `%`);
-
-    // let $axis = $(`<div class="bar-chart-axis"></div>`)
-    //   .on(`click`, () => {
-    //     this.hideAllSortHolders(sortAxisHolder);
-    //     sortAxisHolder.toggleClass(`active-sort-panel`);
-    //   })
-    //   .append($axisTitle)
-    //   .append($(`<div>`).addClass(`max-value`).append($axisMaxValue));
-
-    // this.$el.append($axis);
-
-    // window.requestAnimationFrame(() => { // defer so we can inspect the fully-rendered table
-    //   const tableColWidths = this.$el.parents(`table`)
-    //     .find(`tbody tr:first-child td`).map((i, el) => $(el).outerWidth()).get();
-
-    //   // set header widths
-    //   $headers.each((i, el) => {
-    //     $(el).width(tableColWidths[i]);
-    //   });
-
-    //   // set axis width
-    //   const headerWidths = tableColWidths.slice(0, -1).reduce((sum, width) => sum + width, 0);
-    //   $axis.width(`calc(100% - ${headerWidths}px)`);
-    //   this.$el.removeClass(`loading`);
-    // });
-  }
-
-  createSortIcon(headerIdx) {
-    let sortEl = null;
-    if (
-      this.sortConfig &&
-      this.sortConfig.sortBy === `column` &&
-      !(this.sortConfig.hideFirstSort && headerIdx === 0)
-    ) {
-      const colAttrs = this.sortConfig.colSortAttrs[headerIdx];
-      if (colAttrs) {
-        sortEl = $(`<svg-icon icon="sort-${SORT_ICONS[colAttrs.sortBy]}-${colAttrs.sortOrder}">`);
-      }
-    }
-    return sortEl || $(`<svg-icon empty>`);
-  }
-
-  createAxisSortIcon() {
-    let sortEl = null;
-    if (this.sortConfig) {
-      if (this.sortConfig.colSortAttrs && this.displayOptions && this.displayOptions.plotStyle === `stacked`) {
-        const headerIdx = this.sortConfig.hideFirstSort ? 0 : this.headers.length;
-        const colAttrs = this.sortConfig.colSortAttrs[headerIdx];
-        if (colAttrs) {
-          sortEl = $(`<svg-icon icon="sort-${SORT_ICONS[colAttrs.sortBy]}-${colAttrs.sortOrder}">`);
-        }
-      } else if (this.sortConfig.sortBy === `value`) {
-        sortEl = $(`<svg-icon icon="sort-value-${this.sortConfig.sortOrder}">`);
-      }
-    }
-    return sortEl || $(`<svg-icon empty>`);
+    });
   }
 });
