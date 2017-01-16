19bf38cd7e0379c4373b7ba0267777bf86f48595
teddddd <ted@mixpanel.com>
2017-01-16 02:02:24 +0000

    copy over string-equals filter helpers from old builder

--- src/irb-app/new-irb-builder/builder-filter/builder-screen-filter-property.js
+++ src/irb-app/new-irb-builder/builder-filter/builder-screen-filter-property.js
@@ -3,7 +3,12 @@
 
 import { BuilderScreenBase } from '../builder-pane/builder-screen-base';
 import { FilterClause } from '../../../models/clause';
-import { extend, getIconForPropertyType } from '../../../util';
+import {
+  extend,
+  getIconForPropertyType,
+  removeByValue,
+  renamePropertyValue,
+} from '../../../util';
 
 import template from './builder-screen-filter-property.jade';
 
@@ -27,6 +32,27 @@ document.registerElement(`builder-screen-filter-property`, class extends Builder
         },
         filterOperators: filterType => FilterClause.FILTER_OPERATORS[filterType],
         getActiveClause: () => this.app.hasStageClause() ? this.app.activeStageClause : {},
+
+        // filter content selection
+        allEqualsValuesSelected: () => false,
+        getEqualsMatches: () =>
+          this.getConfig(`helpers`).getValueMatches(this.app.activeStageClause.filterSearch),
+        getValueMatches: (string, invert) =>
+          this.state.topPropertyValues
+            .filter(value => !string || renamePropertyValue(value).toLowerCase().indexOf(string.toLowerCase()) !== -1 ? !invert : !!invert),
+        toggleStringEqualsValueSelected: value => {
+          const clause = this.app.activeStageClause;
+          const selected = (clause && clause.filterValue) || [];
+          let filterValue;
+
+          if (selected.indexOf(value) === -1) {
+            filterValue = [...selected, value];
+          } else {
+            filterValue = removeByValue(selected, value);
+          }
+
+          this.app.updateStageClause({filterValue});
+        },
 
         // dropdowns
         isMenuOpen: menu => {
