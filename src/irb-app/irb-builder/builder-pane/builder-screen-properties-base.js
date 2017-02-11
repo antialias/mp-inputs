06b1717f0008872bca40e426ebc26094e9004694
Cassie <cassie.morford@mixpanel.com>
2017-02-10 18:49:01 -0800

    easier than hotfix?

--- src/irb-app/irb-builder/builder-pane/builder-screen-properties-base.js
+++ src/irb-app/irb-builder/builder-pane/builder-screen-properties-base.js
@@ -11,7 +11,6 @@ export class BuilderScreenPropertiesBase extends BuilderScreenBase {
         clickedResourceType: resourceType => this.app.updateBuilderCurrentScreen({resourceType}),
         getSelectedResourceType: () => this.getSelectedResourceType(),
         getProperties: () => this.getProperties(),
-        getRecentProperties: match => this.getRecentProperties(match),
         getPropertySections: () => this.getPropertySections(),
       }),
     };
@@ -23,13 +22,13 @@ export class BuilderScreenPropertiesBase extends BuilderScreenBase {
     };
   }
 
-  getRecentProperties(match=false) {
+  getMatchedRecentProperties() {
     // todo cassie match then filter
     const resourceType = this.getSelectedResourceType();
     const recentProperties = this.state.recentProperties
       .filter(property => resourceType === Clause.RESOURCE_TYPE_ALL || property.resourceType === resourceType)
       .slice(0, 3);
-    return match ? this.matchingItems(recentProperties, renameProperty) : recentProperties;
+    return this.matchingItems(recentProperties, renameProperty);
   }
 
   getProperties(resourceType=ShowClause.RESOURCE_TYPE_ALL) {
@@ -69,7 +68,7 @@ export class BuilderScreenPropertiesBase extends BuilderScreenBase {
 
     sections.push({
       label: `Recent Properties`,
-      list: this.getRecentProperties(true),
+      list: this.getMatchedRecentProperties(),
     });
 
     if ([ShowClause.RESOURCE_TYPE_EVENTS, ShowClause.RESOURCE_TYPE_ALL].includes(resourceType) && !isPeopleQuery) {
