52820149d4f4983184d4654d318caa2ee3305c7e
Jordan <jordan@mixpanel.com>
2017-02-13 14:47:10 -0800

    support conjunctions in filter section

--- src/models/section.js
+++ src/models/section.js
@@ -12,6 +12,7 @@ import {
   FilterClause,
   TimeClause,
 } from './clause';
+import { extend } from '../util';
 
 export class Section {
   constructor(attrs={}) {
@@ -39,9 +40,18 @@ export class Section {
   }
 
   static deserialize(sectionType, clauseAttrs) {
-    return Section.create(sectionType, {
-      clauses: clauseAttrs.map(c => Clause.create(sectionType, c)),
-    });
+    // debugger;
+    let section = {clauses: []};
+    if (Array.isArray(clauseAttrs)) {
+      section = Section.create(sectionType, {
+        clauses: clauseAttrs.map(c => Clause.create(sectionType, c)),
+      });
+    } else if (typeof clauseAttrs === `object` && Array.isArray(clauseAttrs.clauses)) {
+      section = Section.create(sectionType, extend(clauseAttrs, {
+        clauses: clauseAttrs.clauses.map(c => Clause.create(sectionType, c)),
+      }));
+    }
+    return section;
   }
 
   create() {
@@ -127,12 +137,34 @@ export class GroupSection extends Section {
 GroupSection.TYPE = GroupSection.prototype.TYPE = `group`;
 
 export class FilterSection extends Section {
+  constructor(attrs={}) {
+    super(...arguments);
+    if (attrs && attrs.conjunction && FilterSection.CONJUNCTION_TYPES.includes(attrs.conjunction)) {
+      this.conjunction = attrs.conjunction;
+    } else {
+      this.conjunction = FilterSection.CONJUNCTION_ALL;
+    }
+  }
+
   get valid() {
     return super.valid &&
-      this.clauses.every(clause => clause instanceof FilterClause);
+      this.clauses.every(clause => clause instanceof FilterClause) &&
+      FilterSection.CONJUNCTION_TYPES.includes(this.conjunction);
+  }
+
+  toUrlData() {
+    const clauses = super.toUrlData();
+    return clauses.length ? {clauses, conjunction: this.conjunction} : {};
   }
 }
+
 FilterSection.TYPE = FilterSection.prototype.TYPE = `filter`;
+FilterSection.CONJUNCTION_ALL = FilterSection.prototype.CONJUNCTION_ALL = `all`;
+FilterSection.CONJUNCTION_ANY = FilterSection.prototype.CONJUNCTION_ANY = `any`;
+FilterSection.CONJUNCTION_TYPES = FilterSection.prototype.CONJUNCTION_TYPES = [
+  FilterSection.CONJUNCTION_ALL,
+  FilterSection.CONJUNCTION_ANY,
+];
 
 export class TimeSection extends Section {
   get valid() {
