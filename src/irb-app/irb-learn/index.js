c9925aecb12d1ba1b5be759d51b9a86ac59f1411
Evan Purcer <evanpurcer@gmail.com>
2017-02-15 21:43:31 -0800

    fix z-index issue between learn modals and Mixpanel top nav bar

--- src/irb-app/irb-learn/index.js
+++ src/irb-app/irb-learn/index.js
@@ -4,6 +4,38 @@ import 'mixpanel-common/widgets/tutorial-tooltip';
 import template from './index.jade';
 import './index.styl';
 
+// TODO these are some updates which need to make their way into
+// mixpanel-common and panel soon
+// -------------------------------------------------------------------------------
+const MPButtonBar = window[`mp-common-registered-components`][`mp-button-bar`];
+document.registerElement(`irb-mp-button-bar`, class extends MPButtonBar {
+  attachedCallback() {
+    if (!this.initialized) {
+      super.attachedCallback(...arguments);
+    }
+  }
+});
+
+const MPModal = window[`mp-common-registered-components`][`mp-modal`];
+document.registerElement(`irb-mp-modal`, class extends MPModal {
+  attachedCallback() {
+    if (this.initialized) {
+      return;
+    }
+
+    if (!this.elementMoved) {
+      this.elementMoved = true;
+      document.body.style.position = `relative`;
+      document.body.appendChild(this);
+    }
+
+    if (!this.initialized) {
+      super.attachedCallback(...arguments);
+    }
+  }
+});
+// -------------------------------------------------------------------------------
+
 document.registerElement(`irb-learn`, class extends Component {
   get config() {
     return {
