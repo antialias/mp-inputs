f1a60dd6fbb04462f2dbdc43e901d037a9f4c1ba
teddddd <ted@mixpanel.com>
2017-02-20 17:45:59 -0800

    the most basic of irb-app rendering tests

--- test/browser/irb-app.js
+++ test/browser/irb-app.js
@@ -1,14 +1,21 @@
-console.log(`irb-app test`);
+import initIRB from '../../src/init';
 
 describe(`fully integrated irb-app`, function() {
-  // var el;
+  let app;
 
-  // beforeEach(function() {
-  //   document.body.innerHTML = ``;
-  //   el = document.createElement(`simple-app`);
-  // });
+  beforeEach(function(done) {
+    initIRB().then(IRB => {
+      app = IRB;
+      app.apiSecret = `test_secret`;
+      document.body.innerHTML = ``;
+      document.body.appendChild(app);
+      requestAnimationFrame(() => done());
+    });
+  });
 
-  it(`runs this test`, function() {
-    expect(true).to.eql(true);
+  it(`renders main sections`, function() {
+    for (const section of [`irb-header`, `irb-builder`, `irb-result`, `irb-reports`]) {
+      expect(app.querySelector(section)).to.be.ok;
+    }
   });
 });
