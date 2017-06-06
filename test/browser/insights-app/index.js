e22f1992806f1f1991abe293212d31ea66e07eca
Noj Vek <noj.vek@mixpanel.com>
2017-06-06 11:53:47 -0700

    Seems saucelab is always timing out. Not a great error message.

--- test/browser/insights-app/index.js
+++ test/browser/insights-app/index.js
@@ -7,12 +7,11 @@ import './insights-builder';
 describe(`fully integrated insights-app`, function() {
   beforeEach(async function() {
     this.app = await setupAppAsync();
-    // TODO: Remove this
-    console.error(this.app);
   });
 
   it(`renders main sections`, function() {
     for (const section of [`insights-header`, `insights-builder`, `insights-result`, `insights-reports`]) {
+      console.log(this.app);
       expect(this.app.querySelector(section)).to.be.ok;
     }
   });
